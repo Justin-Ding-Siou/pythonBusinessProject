0aeda87106c3e820260bcd01c6e085054960b5ea
Justin-Ding-Siou <69789527+Justin-Ding-Siou@users.noreply.github.com>
2021-09-07 23:09:15 +0800

    0907 建立 PYCHARM 成功

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-class Person(object):
-
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
-
-    # 访问器 - getter方法
-    @property
-    def name(self):
-        return self._name
-
-    # 访问器 - getter方法
-    @property
-    def age(self):
-        return self._age
-
-    # 修改器 - setter方法
-    @age.setter
-    def age(self, age):
-        self._age = age
-
-    def play(self):
-        if self._age <= 16:
-            print('%s正在玩飞行棋.' % self._name)
-        else:
-            print('%s正在玩斗地主.' % self._name)
+def gcd(x, y):
+    """求最大公约数"""
+    (x, y) = (y, x) if x > y else (x, y)
+    for factor in range(x, 0, -1):
+        if x % factor == 0 and y % factor == 0:
+            return factor
 
 
-def main():
-    person = Person('王大锤', 12)
-    person.play()
-    person.age = 22
-    person.play()
-    # person.name = '白元芳'  # AttributeError: can't set attribute
+def lcm(x, y):
+    """求最小公倍数"""
+    return x * y // gcd(x, y)
 
-
-if __name__ == '__main__':
-    main()
+print('hello world')
