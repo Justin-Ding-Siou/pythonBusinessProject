325b27b81fa374058e0961fe9cfc2d2adb450972
Justin-Ding-Siou <69789527+Justin-Ding-Siou@users.noreply.github.com>
2021-10-05 21:20:54 +0800

    1005 ex02完成

--- Week01/ex2.py
+++ Week01/ex2.py
@@ -20,6 +20,9 @@ punc_table = str.maketrans(dicts)
 new_poem2 = poem2.translate(punc_table)
 print(new_poem2)
 
+
+## 找到標點符號
+
 def lcs(S,T):
     m = len(S) #找到長度
     n = len(T) #找到長度
@@ -47,6 +50,7 @@ for s in ret:
 
 ## 參考 leetcode https://blog.csdn.net/Orientliu96/article/details/103905680
 ## 我還不懂怎麼寫
+## 動態規劃
 
 
 
@@ -73,8 +77,48 @@ print(solution())
 ## 輸入測試的身分證id，找到堆應數字
 ## input: U122093098
 ## output: U = 28，哪裡人? 性別: 男或女
-
 dicts = {'A': '10', 'B': '11', 'C': '12', 'D': '13', 'E': '14', 'F': '15', 'G': '16',
 'H': '17', 'J': '18', 'K': '19', 'L': '20', 'M': '21', 'N': '22', 'P': '23',
 'Q': '24', 'R': '25', 'S': '26', 'T': '27', 'U': '28', 'V': '29', 'W': '30',
 'X': '31', 'Y': '32', 'Z': '33'}
+
+# input 一個英文字母
+ID = input("請輸入你的身分證的大寫英文字母: ")
+
+# 輸入整串身分證字號
+# 輸入是否正確
+
+# 將 dict 給迭代
+for key in dicts:
+    print(key, "->", dicts[key])
+
+# 找出相應的號碼
+for key in dicts:
+    if key == ID:
+        print(key, "->", dicts[key])
+
+# 完整版
+# 代碼表
+idcheck = [10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33]
+TotalVal = 0
+idcode = ""
+while (len(idcode) != 10):
+    idcode = input("輸入身分證號:")  # A123456789
+
+print("身分證:" + idcode)
+CapVal = idcheck[ord(idcode[0]) - 65]  # 查表
+idcode11 = str(CapVal) + idcode[1:10]
+TotalVal = int(idcode11[0])  # 10123456789
+
+for index, item in enumerate(idcode11[1:10]):
+    m = int(item) * (9 - index)
+    TotalVal = TotalVal + m
+
+TotalVal = TotalVal + int(idcode11[10])
+print("身分證是", end='')
+if TotalVal % 10 == 0:
+    print("正確的")
+else:
+    print("錯誤的")
+
+
